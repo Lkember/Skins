cf94b8a1c04a5b49d3af91d403e6f705173661b4
Logan Kember <lkember@uwo.ca>
2020-02-17 22:42:10 -0800

    first commit

---
+++ Skins/Views/StartUpViewController.swift
@@ -0,0 +1,33 @@
+//
+//  StartUpViewController.swift
+//  Skins
+//
+//  Created by Logan Kember on 2020-02-17.
+//  Copyright © 2020 Logan Kember. All rights reserved.
+//
+
+import UIKit
+
+class StartUpViewController: UIViewController {
+
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var statsButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
